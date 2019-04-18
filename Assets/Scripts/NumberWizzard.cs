9bdf7e93579843e6442c31c5847e830c3100560e
VivekGavhane <vivekbgavhane@gmail.com>
2019-04-18 19:14:25 +0530

    Final Commit number wizzard UI
    Final Commit number wizzard UI

---
+++ Assets/Scripts/NumberWizzard.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizzard : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] Text guessText;
+ 
+
+    int guess;
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+        max = max + 1;
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+    }
+}
